36970e5827bc498b1a7d6a18a05624e5fd605a39
김주환 <155719909+Kimjuice1@users.noreply.github.com>
2025-08-09 15:09:35 +0900

    [Bronze IV] Title: 10부제, Time: 168 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int count = 0;
+        for(int i = 0; i < 5; i++)
+        {
+            int B = sc.nextInt();
+            if(A == B)
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
