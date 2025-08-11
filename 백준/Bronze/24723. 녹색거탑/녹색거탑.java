8e6ed2372325f257337341fd8cd5d0510c43a580
김주환 <155719909+Kimjuice1@users.noreply.github.com>
2025-08-11 11:26:02 +0900

    [Bronze IV] Title: 녹색거탑, Time: 184 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int A[] = {1, 2, 4, 8, 16, 32};
+
+        System.out.println(A[N]);
+    }
+}
